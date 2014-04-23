ae8a6aec25249b3c0135a7d31dc5262fa3f01f0a
RobinTang <sintrb@gmail.com>
2014-04-23 17:06:13 +0800

    修改测试

--- test/com/sin/java/plot/test/TestMain.java
+++ test/com/sin/java/plot/test/TestMain.java
@@ -1,7 +1,6 @@
 package com.sin.java.plot.test;
 
 import com.sin.java.plot.Plot;
-import com.sin.java.plot.model.DrawableObject;
 
 public class TestMain {
 
@@ -16,9 +15,7 @@ public class TestMain {
 			y1[i] = Math.sin(i / 10.0f);
 			y2[i] = Math.cos(i / 10.0f);
 		}
-		DrawableObject draw1 = new DrawableObject(y1, "-b");
-		DrawableObject draw2 = new DrawableObject(y2, "-r");
-		Plot.plot(draw1);
-		Plot.plot(draw2);
+		Plot.plot(y1, "-b");
+		Plot.plot(y2, "-r");
 	}
 }
